af52e8b7f170fe2722639004d2a725ae1594f01c
alex raykov <alexraykov2000@gmail.com>
2021-02-10 14:55:16 +0300

    first commit

---
+++ ITMO.ADO.NET.DBConnection/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ITMO.ADO.NET.DBConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        SqlConnection connection = new SqlConnection();
+        string connectionString = GetConnectionStringByName("DBConnect.NorthwindConnectionString");
+
+        //string connectionString = @"Data Source=WS-45\SQLEXPRESS;
+        //                            Initial Catalog=Northwind;
+        //                            Integrated Security=true";
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dBOperationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.diconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asyncConnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectionListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dBOperationsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dBOperationsToolStripMenuItem
+            // 
+            this.dBOperationsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem,
+            this.diconnectToolStripMenuItem,
+            this.asyncConnectToolStripMenuItem,
+            this.connectionListToolStripMenuItem});
+            this.dBOperationsToolStripMenuItem.Name = "dBOperationsToolStripMenuItem";
+            this.dBOperationsToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.dBOperationsToolStripMenuItem.Text = "DB operations";
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.connectToolStripMenuItem.Text = "Connect...";
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.connectToolStripMenuItem_Click);
+            // 
+            // diconnectToolStripMenuItem
+            // 
+            this.diconnectToolStripMenuItem.Name = "diconnectToolStripMenuItem";
+            this.diconnectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.diconnectToolStripMenuItem.Text = "Diconnect...";
+            this.diconnectToolStripMenuItem.Click += new System.EventHandler(this.diconnectToolStripMenuItem_Click);
+            // 
+            // asyncConnectToolStripMenuItem
+            // 
+            this.asyncConnectToolStripMenuItem.Name = "asyncConnectToolStripMenuItem";
+            this.asyncConnectToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.asyncConnectToolStripMenuItem.Text = "Async connect...";
+            this.asyncConnectToolStripMenuItem.Click += new System.EventHandler(this.asyncConnectToolStripMenuItem_Click);
+            // 
+            // connectionListToolStripMenuItem
+            // 
+            this.connectionListToolStripMenuItem.Name = "connectionListToolStripMenuItem";
+            this.connectionListToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.connectionListToolStripMenuItem.Text = "Connection list";
+            this.connectionListToolStripMenuItem.Click += new System.EventHandler(this.connectionListToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dBOperationsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem diconnectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asyncConnectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectionListToolStripMenuItem;
+    }
+
+   
+
+}
+
---
+++ ITMO.ADO.NET.DBConnection/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ITMO.ADO.NET.DBConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string GetConnectionStringByName(string name)
+        {
+            string returnValue = null; ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[name];
+            if (settings != null)
+                returnValue = settings.ConnectionString;
+            return returnValue;
+        }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (
+                    connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = connectionString; connection.Open();
+                    MessageBox.Show("Соединение с базой данных " + connection.Database + " выполнено успешно " + "\nСервер: " + connection.DataSource);
+                }
+                else MessageBox.Show("Соединение с базой данных уже установлено");
+            }
+            catch (SqlException XcpSQL)
+            {
+                foreach (SqlError se in XcpSQL.Errors)
+                {
+                    MessageBox.Show(se.Message, "Источник ошибки: " + se.Source,
+                  MessageBoxButtons.OK,
+                  MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected Exception",
+              MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void diconnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close(); MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else MessageBox.Show("Соединение с базой данных уже закрыто");
+        }
+
+        private async void asyncConnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = connectionString;
+                    await connection.OpenAsync();
+                    MessageBox.Show("Соединение с базой данных " + connection.Database + " выполнено успешно " + "\nСервер: " + connection.DataSource);
+                }
+                else
+                    MessageBox.Show("Соединение с базой данных уже установлено");
+            }
+
+            catch (SqlException XcpSQL)
+            {
+                foreach (SqlError se in XcpSQL.Errors)
+                {
+                    MessageBox.Show(se.Message, "Источник ошибки: " + se.Source,
+                  MessageBoxButtons.OK,
+                  MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected Exception",
+              MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void connection_StateChange(object sender, StateChangeEventArgs e)
+        {
+            this.connectToolStripMenuItem.Enabled = (e.CurrentState == ConnectionState.Closed);
+            this.asyncConnectToolStripMenuItem.Enabled = (e.CurrentState == ConnectionState.Closed);
+            this.diconnectToolStripMenuItem.Enabled = (e.CurrentState == ConnectionState.Open);
+        }
+
+        private void connectionListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    string str = String.Format("Name = {0}\nProviderName = {1}\nConnectionString = {2}", cs.Name, cs.ProviderName, cs.ConnectionString); MessageBox.Show(str, "Параметры подключений");
+                }
+            }
+        }
+    }
+}

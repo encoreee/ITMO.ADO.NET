c5801ab60c451a4e87c395ab32582b710f08ab51
alex raykov <alexraykov2000@gmail.com>
2021-02-17 21:27:16 +0300

    les 9_1

---
+++ ITMO.ADO.NET.CourseManager/CourseViewer.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace ITMO.ADO.NET.CourseManager
+{
+    partial class CourseViewer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.departmentList = new System.Windows.Forms.ComboBox();
+            this.courseGridView = new System.Windows.Forms.DataGridView();
+            this.saveChanges = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // departmentList
+            // 
+            this.departmentList.FormattingEnabled = true;
+            this.departmentList.Location = new System.Drawing.Point(12, 12);
+            this.departmentList.Name = "departmentList";
+            this.departmentList.Size = new System.Drawing.Size(148, 24);
+            this.departmentList.TabIndex = 0;
+            this.departmentList.SelectedIndexChanged += new System.EventHandler(this.departmentList_SelectedIndexChanged);
+            // 
+            // courseGridView
+            // 
+            this.courseGridView.AllowDrop = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.courseGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.courseGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.courseGridView.DefaultCellStyle = dataGridViewCellStyle5;
+            this.courseGridView.Location = new System.Drawing.Point(13, 43);
+            this.courseGridView.Name = "courseGridView";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.courseGridView.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.courseGridView.RowHeadersWidth = 51;
+            this.courseGridView.RowTemplate.Height = 24;
+            this.courseGridView.Size = new System.Drawing.Size(404, 347);
+            this.courseGridView.TabIndex = 1;
+            // 
+            // saveChanges
+            // 
+            this.saveChanges.Location = new System.Drawing.Point(12, 405);
+            this.saveChanges.Name = "saveChanges";
+            this.saveChanges.Size = new System.Drawing.Size(75, 23);
+            this.saveChanges.TabIndex = 2;
+            this.saveChanges.Text = "Save";
+            this.saveChanges.UseVisualStyleBackColor = true;
+            this.saveChanges.Click += new System.EventHandler(this.saveChanges_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add row";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CourseViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.saveChanges);
+            this.Controls.Add(this.courseGridView);
+            this.Controls.Add(this.departmentList);
+            this.Name = "CourseViewer";
+            this.Text = "Course Viewer";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CourseViewer_FormClosed);
+            this.Load += new System.EventHandler(this.CourseViewer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox departmentList;
+        private System.Windows.Forms.DataGridView courseGridView;
+        private System.Windows.Forms.Button saveChanges;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ITMO.ADO.NET.CourseManager/CourseViewer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace ITMO.ADO.NET.CourseManager
+{
+    public partial class CourseViewer : Form
+    {
+
+        private SchoolEntities schoolContext;
+        public CourseViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void CourseViewer_Load(object sender, EventArgs e)
+        {
+            schoolContext = new SchoolEntities();
+            //var departments = from d in schoolContext.Departments.Include("Courses")
+            //                  orderby d.Name
+            //                  select d;
+
+            var departments = schoolContext.Departments.Include(x => x.Courses).OrderBy(x => x.Name);
+            
+
+            try
+            {
+                this.departmentList.DisplayMember = "Name";
+                this.departmentList.DataSource = departments.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void departmentList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Department department = (Department)this.departmentList.SelectedItem;
+                courseGridView.DataSource = department.Courses.ToList();
+             
+                courseGridView.Columns["Department"].Visible = false;
+                courseGridView.Columns["StudentGrades"].Visible = false;
+                courseGridView.Columns["OnlineCourse"].Visible = false;
+                courseGridView.Columns["OnsiteCourse"].Visible = false;
+                courseGridView.Columns["People"].Visible = false;
+                courseGridView.Columns["DepartmentId"].Visible = false;
+                courseGridView.AllowUserToDeleteRows = false;
+                courseGridView.AllowUserToAddRows = true;
+
+                courseGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void saveChanges_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                schoolContext.SaveChanges();
+                MessageBox.Show("Changes saved to the database.");
+                this.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CourseViewer_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Close();
+            schoolContext.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            this.courseGridView.Rows.Add();
+   
+        }
+    }
+}
